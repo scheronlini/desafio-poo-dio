d3d2317b5b8e21bf940b6573ed2e7369092caa99
scheronlini <scheronlini@gmail.com>
2023-01-08 19:26:27 -0300

    Acrescentado Informações pessoais do dev

---
+++ src/br/com/dio/desafio/dominio/InformacoesPessoais.java
@@ -0,0 +1,97 @@
+package br.com.dio.desafio.dominio;
+
+import java.time.LocalDate;
+
+public class InformacoesPessoais {
+
+    private String nome;
+    private LocalDate nascimento;
+    private String estado;
+    private String cidade;
+    private int cep;
+    private String rua;
+    private int numeroCasa;
+    private String email;
+    private String github;
+    private String linkedin;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public LocalDate getNascimento() {
+        return nascimento;
+    }
+
+    public void setNascimento(LocalDate nascimento) {
+        this.nascimento = nascimento;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+    public String getCidade() {
+        return cidade;
+    }
+
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
+
+    public int getCep() {
+        return cep;
+    }
+
+    public void setCep(int cep) {
+        this.cep = cep;
+    }
+
+    public String getRua() {
+        return rua;
+    }
+
+    public void setRua(String rua) {
+        this.rua = rua;
+    }
+
+    public int getNumeroCasa() {
+        return numeroCasa;
+    }
+
+    public void setNumeroCasa(int numeroCasa) {
+        this.numeroCasa = numeroCasa;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getGithub() {
+        return github;
+    }
+
+    public void setGithub(String github) {
+        this.github = github;
+    }
+
+    public String getLinkedin() {
+        return linkedin;
+    }
+
+    public void setLinkedin(String linkedin) {
+        this.linkedin = linkedin;
+    }
+}
